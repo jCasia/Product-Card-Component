afc7c76e88813bcee2227707856ab5950b4bb1bb
jCasia <casjay70@gmail.com>
2023-02-24 00:15:44 +0000

    Added responsive and Readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello Again!😊
+
+## Overview
+
+This is yet another simple card component that was built using React and CSS for practicing the use of props
+
+### Links
+
+- My Frontend Mentor Username - [@jCasia](https://www.frontendmentor.io/profile/jCasia)
+
+### Some screenshot of this project:
+
+![](./src/images/screenshot1.png)
+![](./src/images/screenshot2.png)
--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import "./CardContent.css";
 import Button from "./Button";
 import Price from "./Price";
-import desktopImg from "../images/image-product-desktop.jpg";
 
 const CardContent = (props) => {
   return (
     <>
-      <div className="img-container">
-        <img src={desktopImg} className="img" alt=""></img>
-      </div>
+      <div className="img-container"></div>
       <div className="content">
         <p className="subheading">{props.subheading}</p>
         <h1 className="heading">{props.heading}</h1>
